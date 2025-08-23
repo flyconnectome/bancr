384ebfb97928526b61d1ef785716ec5f890a1bcb
Gregory Jefferis <jefferis@gmail.com>
2025-08-23 12:34:03 +0100

    suppress warning in banc_meta test

--- tests/testthat/test-banc-coconatfly.R
+++ tests/testthat/test-banc-coconatfly.R
@@ -3,7 +3,8 @@ skip_if_not_installed('coconatfly')
 test_that("coconatfly integration works", {
   register_banc_coconat()
   library(coconatfly)
-  bm=try(banc_meta())
+  # suppress warnings as there is one about parsing ngl scenes
+  bm=try(suppressWarnings(banc_meta()))
   skip_if(inherits(bm, 'try-error'),
           message = 'Skipping coconatfly tests as failed to read banc metadata!')
 
