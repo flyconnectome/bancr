65dac9aacfe68d1d167da62f2f39d06c5d44ef1e
Alex Bates <alexander.shakeel.bates@gmail.com>
2026-01-20 17:05:01 -0500

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,13 @@
 library(testthat)
 library(bancr)
+if (identical(Sys.getenv("BANCR_ON_CI"), "true")) {
+  # Only run this on CI
+  simple_python("basic")
 
-simple_python("basic")
-banc_set_token()
+  # Use the path where your workflow writes the secret
+  token_path <- path.expand("~/.cloudvolume/secrets/chunkedgraph-secret.json")
+  if (file.exists(token_path)) {
+    try(bancr::banc_set_token(token_path), silent = TRUE)
+  }
+}
 test_check("bancr")
